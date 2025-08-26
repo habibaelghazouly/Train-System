1d798d3b050974ccd471a1061c0aa7773f0edb40
habibaelghazouly <habibahoussam7@gmail.com>
2025-08-26 15:38:01 +0300

    Schema Models

--- Index.ts
+++ Index.ts
@@ -1,12 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+
 import { createStation, getAllStations } from "./models/station";
 import { createTrain, getAllTrains } from "./models/train";
 import { createTrip, getAllTrips } from "./models/trip";
 
 async function main() {
 
-  console.log(await getAllStations());
-  console.log(await getAllTrains());
-  console.log(await getAllTrips());
+  console.log("Stations:", await getAllStations());
+  console.log("Trains:", await getAllTrains());
+  console.log("Trips:", await getAllTrips());
 }
 
-main().catch(console.error);
+main()
+  .catch(console.error)
+  .finally(async () => await (await import("./prisma")).default.$disconnect());
+
+export default prisma;
