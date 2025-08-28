4b3abce80a9b34317a64170fe74dccbd136174e0
habibaelghazouly <habibahoussam7@gmail.com>
2025-08-28 15:37:06 +0300

    Add postman collection

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,14 +1,9 @@
-import path from "node:path";
-import { defineConfig } from "prisma/config";
-import dotenv from "dotenv";
-
-// Load environment variables
-dotenv.config({ path: path.join(__dirname, ".env") });
-
+import "dotenv/config";
+import { defineConfig } from "@prisma/config";
+ 
 export default defineConfig({
-  schema: path.join(__dirname, "prisma", "schema.prisma"),
+  schema: "./prisma",
   migrations: {
-    path: path.join(__dirname, "prisma", "migrations"),
-    seed: "ts-node ./prisma/seeds/seed.ts",
+    seed: "ts-node prisma/seed.ts",
   },
 });
