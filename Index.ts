e74ef6a47970a2b2625b1dadf2509bd7961fbb11
habibaelghazouly <habibahoussam7@gmail.com>
2025-09-01 15:15:02 +0300

    Add routes

--- Index.ts
+++ Index.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import stationRoutes from "./routes/stations.routes";
 import tripRoutes from "./routes/trips.routes";
-
+import newStationRoutes from "./routes/newStation.routes";
+import newTrainRoutes from "./routes/newTrain.routes";
+import trainRoutes from "./routes/trains.routes";
 
 const app = express();
 
@@ -10,5 +12,8 @@ app.use(express.json());
 // Routes
 app.use("/all", stationRoutes);
 app.use("/trips", tripRoutes);
+app.use("/new", newStationRoutes);
+app.use("/new", newTrainRoutes);
+app.use("/all", trainRoutes);
 
 export default app;
