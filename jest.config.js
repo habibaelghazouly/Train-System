5c37ce6be3018dcc7d378d7ae71cd9ebddbe45cd
habibaelghazouly <habibahoussam7@gmail.com>
2025-09-03 13:54:31 +0300

    Code coverage percent

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,19 @@ module.exports = {
   preset: "ts-jest",
   testMatch: ["**/*.test.ts"],
 
+  collectCoverage: true,
+  coverageReporters: ["html", "text", "lcov"], // html creates coverage folder
+  coverageDirectory: "coverage",
+  // optionally specify which files to collect coverage from
+   collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageReporters: ["html", "text", "lcov"],
+  collectCoverageFrom: [
+    "services/**/*.ts", // include your services
+    "controllers/**/*.ts",
+    "!**/__tests__/**"
+  ],
+
    reporters: [
     "default",
     ["jest-html-reporter", {
