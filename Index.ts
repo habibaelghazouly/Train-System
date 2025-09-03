f2c6c47720f97fef014a1cd76a823dbcd40f1e5a
habibaelghazouly <habibahoussam7@gmail.com>
2025-09-03 13:30:54 +0300

    Swagger : API documentation

--- Index.ts
+++ Index.ts
@@ -3,9 +3,15 @@ import stationRoutes from "./routes/stations.routes";
 import tripRoutes from "./routes/trips.routes";
 import trainRoutes from "./routes/trains.routes";
 
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger/swagger.json";
+
 const app = express();
 
 app.use(express.json());
+
+// API documentation
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Station routes
 app.use("/stations", stationRoutes);
