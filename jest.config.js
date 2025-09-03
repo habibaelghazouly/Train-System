73e56985906b68c324d17d4f82f6034c7e82e030
habibaelghazouly <habibahoussam7@gmail.com>
2025-09-03 12:19:50 +0300

    Test Report

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,15 @@
 module.exports = {
   preset: "ts-jest",
   testMatch: ["**/*.test.ts"],
+
+   reporters: [
+    "default",
+    ["jest-html-reporter", {
+      outputPath: "./reports/jest-report.html",
+      pageTitle: "Jest Test Report",
+      includeFailureMsg: true,
+      includeConsoleLog: true,
+      theme: "dark"
+    }]
+  ]
 };
